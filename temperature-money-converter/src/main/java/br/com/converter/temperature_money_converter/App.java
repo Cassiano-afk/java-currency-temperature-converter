f635e3a0c33845d4cb95f1779b8eaba704504410
Cassiano <cassiano.almeida2003@gmail.com>
2025-04-07 22:01:34 -0300

    Creating Java-Maven project.

---
+++ temperature-money-converter/src/main/java/br/com/converter/temperature_money_converter/App.java
@@ -0,0 +1,10 @@
+package br.com.converter.temperature_money_converter;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
